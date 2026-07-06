fbb0a0d49fc8cf878fa3822e0792f6536cb6976c
cubedhuang <99danielh@gmail.com>
2026-07-06 15:52:28 -0700

    status page

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,5 @@
 import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
-import { z } from "astro/zod";
 
 const words = defineCollection({
   loader: glob({ base: "./words", pattern: "**/*.{md,mdx}" }),
